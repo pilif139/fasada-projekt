482925a93a3822369cf0c41e397fdbd964a59d4e
Filip Kasperski <pifilif@gmail.com>
2024-09-23 11:23:13 +0200

    init commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,3 @@
-<h1 class="text-4xl text-red-900 font-mono">Fasada</h1>
+<main class=" bg-gradient-to-t from-slate-700 to-slate-900 w-screen h-screen flex flex-col items-center py-4">
+    <h1 class="text-4xl text-blue-800 font-nunito font-bold">Fasada</h1>
+</main>
