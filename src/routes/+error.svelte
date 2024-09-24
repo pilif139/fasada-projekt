98e3e4b750107e33a96e706106db39cff89e34ba
Filip Kasperski <pifilif@gmail.com>
2024-09-24 15:42:08 +0200

    answering questions, custom error page.

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import {page} from "$app/stores";
+</script>
+
+<div class="m-auto text-center">
+    <h1 class="font-extrabold text-4xl">Error {$page.status}</h1>
+    <h2 class="text-2xl">{$page.error?.message}</h2>
+</div>
--- src/routes/question/[id]/+page.svelte
+++ src/routes/question/[id]/+page.svelte
@@ -1,27 +1,49 @@
 <script lang="ts">
-    export let data;
-    const question = data.question;
+  export let data;
+  const question = data.question;
+  let currentAns: number | null = null;
+  let completed = false;
 
-    const handleAnswer = (e: MouseEvent, index: number) => {
-       const target = e.target as HTMLButtonElement;
-       const answer = target.innerText;
-       if(index === question.correctAnswer) {
-           
-       } else {
-           alert('Incorrect!');
-       }
+  const handleAnswer = (e: MouseEvent, index: number) => {
+    if(!completed){
+      const target = e.target as HTMLButtonElement;
+      currentAns = index;
+      completed=true;
+
+      // add points to the user
+      if (index === question.correctAnswer) {
+      } else {
+      }
     }
+  };
 </script>
 
 <main class="flex flex-col items-center gap-4 w-[20vw]">
   <h1>{question.question}</h1>
   <ul class="flex justify-between">
-    {#each question.answers as answer,index}
+    {#each question.answers as answer, index}
+      {#if currentAns === question.correctAnswer && currentAns === index}
         <button
-            on:click={(e)=>handleAnswer(e,index)}
-            class="bg-gradient-to-t from-slate-950 to-black p-4 rounded-xl mx-2">
-            {answer}
+          on:click={(e) => handleAnswer(e, index)}
+          class="bg-gradient-to-t from-green-800 to-black p-4 rounded-xl mx-2"
+        >
+          {answer}
         </button>
+      {:else if index !== question.correctAnswer && currentAns !== null}
+        <button
+        on:click={(e) => handleAnswer(e, index)}
+        class="bg-gradient-to-t from-red-800 to-black p-4 rounded-xl mx-2"
+      >
+        {answer}
+      </button>
+      {:else}
+        <button
+          on:click={(e) => handleAnswer(e, index)}
+          class="bg-gradient-to-t from-slate-950 to-black p-4 rounded-xl mx-2"
+        >
+          {answer}
+        </button>
+      {/if}
     {/each}
   </ul>
 </main>
