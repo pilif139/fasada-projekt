35068b909958f9584fbfafb87279f8db713b7de1
pilif139 <pifilif@gmail.com>
2024-11-03 15:06:09 +0100

    button refactor to svelte 5

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,28 +1,16 @@
 <script lang="ts">
     type ButtonProps = {
-        className: string;
-        href: string;
-        onClick: (() => void) | null;
+        className?: string;
+        href?: string;
+        onClick?: (() => void) | null;
         children: any;
     }
     let { className = "", href = "", onClick = null, children }: ButtonProps = $props();
 </script>
 
-{#if href}
-    <a  href={href} 
-        class="bg-blue-500 px-8 py-4 rounded-xl text-xl font-bold hover:bg-blue-800 transition-all border-2 border-blue-600 hover:scale-110 duration-300 {className}">
-        {@render children()}
-    </a>
-{:else if onClick}
-    <button onclick={onClick} 
-            class="bg-blue-500 px-8 py-4 rounded-xl text-xl font-bold hover:bg-blue-800      transition-all border-2 border-blue-600 hover:scale-110 duration-300 {className}"
-            >
-        {@render children()}
-    </button>
-{:else}
-    <button
-        class="bg-blue-500 px-8 py-4 rounded-xl text-xl font-bold hover:bg-blue-800 transition-all border-2 border-blue-600 hover:scale-110 duration-300 {className}"
-        >
-        {@render children()}
-    </button>
-{/if}
+
+<a  href={href}
+    onclick={onClick}
+    class="bg-blue-500 px-8 py-4 rounded-xl text-xl font-bold hover:bg-blue-800 transition-all border-2 border-blue-600 hover:scale-110 duration-300 {className}">
+    {@render children()}
+</a>
