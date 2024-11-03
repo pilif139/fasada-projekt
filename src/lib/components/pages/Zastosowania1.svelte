80786e5f2ea3ecf3ed0af18071abdac97b578f7b
pilif139 <pifilif@gmail.com>
2024-11-03 21:40:56 +0100

    zdjecia do stron z zastosowaniami

--- src/lib/components/pages/Zastosowania1.svelte
+++ src/lib/components/pages/Zastosowania1.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
     import Heading from "../Heading.svelte";
+    import src from "$lib/assets/infolinia.png";
 </script>
 
 <Heading>Zastosowania - Sklep</Heading>
 <p> Gdy dzwonisz do sklepu, aby złożyć zamówienie, biuro jest twoją fasadą dla wszystkich usług i oddziałów tego sklepu. Pracownik sklepu, czy automat zgłoszeniowy, stanowią prosty interfejs głosowy do systemu zamawiania, płacenia i różnych usług dostawczych.</p>
-
+<img {src} alt="zastosowanie sklep" class="rounded-xl shadow-xl shadow-gray-900 h-[50vh]">
--- src/lib/components/pages/Zastosowania3.svelte
+++ src/lib/components/pages/Zastosowania3.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import Heading from "../Heading.svelte";
+    import src from "$lib/assets/kalkulator.png";
 </script>
 
 <Heading>Zastosowania - Kalkulator</Heading>
 <p>       Przykładem użycia wzorca fasady może być aplikacja bankomatowa, która musi wchodzić w interakcję z systemem bankowym. Skoro aplikacja bankomatowa wykorzystuje tylko niewielką część możliwości systemu bankowego (autoryzacja karty, sprawdzenie stanu konta, wypłacanie i wpłacanie), to można zastosować fasadę, która zasłoni skomplikowaną strukturę wewnętrzną systemu bankowego. Upraszcza to pisanie aplikacji na bankomaty, a jednocześnie zapewnia lepsze bezpieczeństwo systemu bankowego. .</p>
+<img {src} alt="zastosowanie kalkulator" class="rounded-xl shadow-xl shadow-gray-900 h-[50vh]">
